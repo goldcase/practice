a4384608d89426a4f7e608423ac225a1e1813240
Johnny Chang <johnny@johnnychang.com>
2015-05-29 16:29:49 -0700

    solvd 2-4

--- 2-4.py
+++ 2-4.py
@@ -2,7 +2,28 @@
 
 from classes.LinkedList import *
 
+linked_list = create_rand_list(15)
+
 # Write code to partition a linked list around a value x
 
 def partition_list(l, x):
-    return
+    less    = LinkedList()
+    greater = LinkedList()
+
+    curr = l.head
+    while curr.next != None:
+        if curr.value < x:
+            less.add_node(curr.value)
+        else:
+            greater.add_node(curr.value)
+        curr = curr.next
+
+    less.tail.next = greater.head
+    less.tail = greater.tail
+    
+    return less
+
+# test
+print("Random Linked List: " + str(linked_list))
+print("Partition around 0")
+print(partition_list(linked_list, 0))
